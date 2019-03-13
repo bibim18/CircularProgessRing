9ac557f2c35c735c33ce45cc923e3dd43606f2b6
Admin <admin@MAC3.local>
2019-03-13 12:16:25 +0700

    Set value

--- CircularProcessRing/ViewController.swift
+++ CircularProcessRing/ViewController.swift
@@ -1,14 +1,6 @@
-//
-//  ViewController.swift
-//  Example
-//
-//  Created by Luis Padron on 9/16/16.
-//  Copyright © 2016 Luis Padron. All rights reserved.
-//
-
 import UIKit
 import UICircularProgressRing
-                                        // If you'd prefer to use a delegate then you can implement it and override func finishedUpdatingProgressFor(_ ring: UICircularProgressRingView)
+
 class ViewController: UIViewController, UICircularProgressRingDelegate  {
     
 
@@ -18,29 +10,17 @@ class ViewController: UIViewController, UICircularProgressRingDelegate  {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Customize some properties
         ring1.animationStyle = kCAMediaTimingFunctionLinear
         ring1.font = UIFont.systemFont(ofSize: 70)
         ring2.fontColor = UIColor.gray
         ring3.maxValue = 10
         
-        // Set the delegate
         ring1.delegate = self
         ring2.delegate = self
         
     }
     
-    // Button clicked, animate them views
     @IBAction func animateTheViews(_ sender: AnyObject) {
-        // Animate the views
-        /* 
-         Send in the value you would like to update the view with.
-         animationDuration when set to 0 causes view to not be animated.
-         Optionally you can also supply a completion handler
-         */
-        
-        // You can set the animationStyle like this
         ring1.animationStyle = kCAMediaTimingFunctionLinear
         ring1.setProgress(value: 99, animationDuration: 5, completion: nil)
         
@@ -52,8 +32,6 @@ class ViewController: UIViewController, UICircularProgressRingDelegate  {
                 print("Ring 2 finished")
             }
         }
-        
-        // This has a max value of 10 so we set this accordingly and the view calculates how the progress should look
         ring3.setProgress(value: 6.52, animationDuration: 6)
         
     }
